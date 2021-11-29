abe8e5cbb8a0da8023aecb70d7d6658a9d7358b2
Rama <nseorama@gmail.com>
2021-11-29 14:55:58 +0200

    add search functionalities with many site

--- search_engine/search_engine/spiders/search_spider.py
+++ search_engine/search_engine/spiders/search_spider.py
@@ -1,61 +1,33 @@
 import scrapy
-# from io import StringIO
-# from functools import partial
-# from scrapy.http import Request
-# from scrapy.spiders import BaseSpider
-from scrapy.spiders import CrawlSpider, Rule
-# from scrapy.linkextractors import LinkExtractor
-# from scrapy.item import Item
 
 
-class SearchSpider(CrawlSpider):
+class SearchSpider(scrapy.Spider):
     name = "search"
 
-    # allowed_domains = [
-    #     'https://bk.rw/',
-    #     'https://rwanda.accessbankplc.com/',
-    #     'https://www.ecobank.com/'
-    # ]
+    allowed_domains = [
+        'https://bk.rw/',
+        'https://rwanda.accessbankplc.com/',
+        'https://www.ecobank.com/'
+    ]
 
-    print('============================================')
     start_urls = [
         'https://bk.rw/',
-        # 'https://rwanda.accessbankplc.com/',
-        # 'https://www.ecobank.com/rw/personal-banking/countries'
+        'https://www.ecobank.com/'
+        ''
     ]
-    # rules = [Rule(LinkExtractor(), follow=True, callback="search_word")]
 
-    # crawl_count = 0
-    # words_found = 0
+    def parse(self, response):
 
-    print('============================================')
+        # start_urls = [
+        #     'https://bk.rw/',
+        #     'https://rwanda.accessbankplc.com/',
+        #     'https://www.ecobank.com/'
+        # ]
 
-    def search_word(self, response):
-
-        # self.__class__.crawl_count += 1
-        # crawl_count = self.__class__.crawl_count
-
-        wordlist = [
-            'loan',
-            # 'money transfer'
-        ]
-
-        url = response.url
-        print('============', url)
-        contenttype = response.headers.get(
-            'content_type', '').decode('utf-8').lower()
-        data = response.body.decode('utf-8')
-
-        # print(data)
-
-        for word in wordlist:
-            substrings = find_all_substrings(data, word)
-            for pos in substrings:
-                ok = False
-                if not ok:
-                    self.__class__.words_found += 1
-                    print(word + ";" + url + ";")
-        print('=======================================')
-        print(Item())
-        print('=======================================')
-        return Item()
+        # for link in start_urls:
+        print('============================================',
+              response.xpath(
+                  "//*[contains(text(), 'savings')]").getall(),
+              '============================================',
+              response.url
+              )
